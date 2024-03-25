6900d849f0d0b88ab674cb03c37401b3ea90c472
Daniel <94477022+NostalgicDani@users.noreply.github.com>
2024-03-24 20:02:43 -0700

    Added hamburger for mobile devices & changed how quotes are done

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ window.onscroll = function () {
   progressScroll();
 };
 
-let slideIndex = 0;
-
-document.addEventListener("DOMContentLoaded", function () {
-  carousel();
-});
 
 function progressScroll() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -16,18 +11,50 @@ function progressScroll() {
   document.getElementById("progressScroll").style.width = scrolled + "%";
 }
 
-function bgScroll() {
+document.addEventListener("DOMContentLoaded", function () {
+  carousel();
+});
 
-}
-
+let slideIndex = 0;
+const quotes = [
+  "I knew a man once who said, 'Death smiles at us all. All a man can do is smile back.'",
+  "This player dreamed of sunlight and trees. Of fire and water. It dreamed it created.",
+  "And the player was the universe. And the player was love.",
+  "I would have gone with you to end, to the very fires of Mordor.",
+  "Open your heart.",
+  "Not all those who wander are lost.",
+  "The world is not in your books and maps. It's out there.",
+  "People call those imperfections, but no, that’s the good stuff.",
+  "But now I'm not so sure I believe in beginnings and endings. There are days that define your story beyond your life."
+]
+const authors = [
+  "— Maximus, <em>Gladiator</em>",
+  "— Julian Gough, <em>Minecraft</em>",
+  "— Julian Gough, <em>Minecraft</em>",
+  "— Aragorn, <em>The Lord of the Rings</em>",
+  "— Kratos, <em>God of War: Ragnarok</em>",
+  "— Bilbo Baggins, <em>The Lord of the Rings</em>",
+  "— Gandalf, <em>The Hobbit</em>",
+  "— Sean, <em>Good Will Hunting</em>",
+  "— Louise, <em>The Arrival</em>"
+]
 function carousel() {
-  let x = document.getElementsByClassName("quote");
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
+  let quote = document.getElementById("quote");
+  let author = document.getElementById("author");
+  quote.style.animation = 'none'; //disable animation
+  author.style.animation = 'none'; //disable animation
+
+  setTimeout(() => { 
+    quote.textContent = quotes[slideIndex]; //set text to quote
+    author.innerHTML = authors[slideIndex]; // Set text to author
+    quote.style.animation = ''; //enable animation after text change
+    author.style.animation = ''; //enable animation after text change
+
+  }, 10); 
+  
   slideIndex++;
-  if (slideIndex > x.length) slideIndex = 1;
-  x[slideIndex - 1].style.display = "block";
+  if (slideIndex >= quotes.length) slideIndex = 0;
+  
   setTimeout(carousel, 6000);
 }
 
@@ -54,5 +81,11 @@ const appearOnScroll = new IntersectionObserver(
 
 fadeIn.forEach((fade) => {
   appearOnScroll.observe(fade);
+});
+
+ document.getElementById("hamburger").addEventListener("click", function() {
+  const mobilebar = document.getElementById("mobilebar");
+  mobilebar.classList.toggle("show");
+  this.classList.toggle("active"); 
 });
 
