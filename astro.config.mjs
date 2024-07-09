ccb505bdd2d0b0b2b430be0db8fba300b0420943
Daniel <daniel.mamuza@gmail.com>
2024-07-08 23:06:02 -0700

    sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://danielmamuza.com",
   base: "/",
-  integrations: [react(), tailwind(), playformCompress()],
+  integrations: [react(), tailwind(), playformCompress(), sitemap()]
 });
