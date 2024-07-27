9765837e024957bbd906328b082a6fdba3ba9f2f
Daniel <daniel.mamuza@gmail.com>
2024-07-27 11:32:13 -0700

    filter out old portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
@@ -9,5 +9,10 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://danielmamuza.com",
   base: "/",
-  integrations: [react(), tailwind(), playformCompress(), sitemap()]
+  integrations: [
+    react(),
+    tailwind(),
+    playformCompress(),
+    sitemap({ filter: (page) => page !== "https://danielmamuza.com/old/" }),
+  ],
 });
