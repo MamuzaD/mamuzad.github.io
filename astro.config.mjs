b3a3db29c0c462689add2151e0fa067e1a8ca07d
Daniel Mamuza <94477022+NostalgicDani@users.noreply.github.com>
2024-12-20 23:39:50 -0800

    spiderman

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,12 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import playformCompress from "@playform/compress";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://danielmamuza.com",
-  base: "/",
-  integrations: [
-    react(),
-    tailwind(),
-    playformCompress(),
-    sitemap({
-      filter: (page) =>
-        page !== "http://danielmamuza.com/old/" &&
-        page !== "https://danielmamuza.com/old/",
-    }),
-  ],
-  output: "hybrid",
-  adapter: vercel({ webAnalytics: { enabled: true } }),
+  integrations: [react(), tailwind()],
+  adapter: vercel()
 });
